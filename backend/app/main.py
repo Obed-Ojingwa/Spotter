4e34b7f1f12e3667ed34b50eba11151de9fa897f
Obed-Ojingwa <obedojingwa@gmail.com>
2026-04-20 11:06:32 +0100

    Exposed localhost for testing using ipv6

--- backend/app/main.py
+++ backend/app/main.py
@@ -48,7 +48,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:3001", "http://127.0.0.1:3000"],
+    allow_origins=["http://localhost:3000", "http://localhost:3001", "http://127.0.0.1:3000", "http://192.168.1.127:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
