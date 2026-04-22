3153a67683bb3894970f13561cbd1216b0562a2c
Obed-Ojingwa <obedojingwa@gmail.com>
2026-04-22 14:56:56 +0100

    Fixed db issue with supabase --- Ready to deploy

--- backend/app/database.py
+++ backend/app/database.py
@@ -4,13 +4,15 @@ from app.config import settings
 import ssl
 
 ssl_context = ssl.create_default_context()
+ssl_context.check_hostname = False
+ssl_context.verify_mode = ssl.CERT_NONE
 
 engine = create_async_engine(
     settings.DATABASE_URL,
     echo=settings.APP_ENV == "development",
     pool_pre_ping=True,
-    pool_size=10,
-    max_overflow=20,
+    pool_size=5,
+    max_overflow=10,
     connect_args={"ssl": ssl_context},
 )
 
@@ -20,10 +22,8 @@ AsyncSessionLocal = async_sessionmaker(
     expire_on_commit=False,
 )
 
-
 class Base(DeclarativeBase):
     pass
-
 
 async def get_db():
     async with AsyncSessionLocal() as session:
@@ -35,3 +35,47 @@ async def get_db():
             raise
         finally:
             await session.close()
+
+
+
+# from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+# from sqlalchemy.orm import DeclarativeBase
+# from app.config import settings
+# import ssl
+# import certifi 
+
+
+
+# ssl_context = ssl.create_default_context()
+# ssl_context = ssl.create_default_context(cafile=certifi.where())
+
+# engine = create_async_engine(
+#     settings.DATABASE_URL,
+#     echo=settings.APP_ENV == "development",
+#     pool_pre_ping=True,
+#     pool_size=10,
+#     max_overflow=20,
+#     connect_args={"ssl": True},
+# )
+
+# AsyncSessionLocal = async_sessionmaker(
+#     engine,
+#     class_=AsyncSession,
+#     expire_on_commit=False,
+# )
+
+
+# class Base(DeclarativeBase):
+#     pass
+
+
+# async def get_db():
+#     async with AsyncSessionLocal() as session:
+#         try:
+#             yield session
+#             await session.commit()
+#         except Exception:
+#             await session.rollback()
+#             raise
+#         finally:
+#             await session.close()
