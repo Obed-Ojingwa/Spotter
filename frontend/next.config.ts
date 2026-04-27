d25ae75e0955813561640ec9af341062f4e439fb
Obed-Ojingwa <obedojingwa@gmail.com>
2026-04-27 18:17:47 +0100

    Disabled Turbopack

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,13 +2,14 @@ import type { NextConfig } from "next";
 
 const withPWA = require("next-pwa")({
   dest: "public",
-  register: true,      // auto register service worker
-  skipWaiting: true,   // activate SW immediately
-  disable: process.env.NODE_ENV === "development", // disable in dev
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
 });
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  webpack: () => ({}), // Disable Turbopack, use webpack instead
 };
 
 export default withPWA(nextConfig);
